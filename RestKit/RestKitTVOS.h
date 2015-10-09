aa89d46e80668d1881023bd7dc3e04f8ae29fae1
Dmytro Taradaika <taradayka.d@gmail.com>
2015-10-09 15:13:08 +0300

    added files

---
+++ RestKit/RestKitTVOS.h
@@ -0,0 +1,18 @@
+//
+//  RestKit.h
+//  RestKit
+//
+//  Created by Dmytro Taradaika on 10/8/15.
+//  Copyright © 2015 Dmytro Taradaika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RestKit.
+FOUNDATION_EXPORT double RestKitVersionNumber;
+
+//! Project version string for RestKit.
+FOUNDATION_EXPORT const unsigned char RestKitVersionString[];
+
+#import "RestKit.h"
+
